2b567ddf7cf7c5f7d69fe5f024b699688ee884e2
Biddls <thomasbiddlecombe@gmail.com>
2023-07-27 14:10:05 +0200

    remove DB

--- src/codeParsing/addrLookup.py
+++ src/codeParsing/addrLookup.py
@@ -1,8 +1,10 @@
 import json
+# import evmDasm
 import requests
-import evmdasm as evmDasm
+import evmdasm
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -21,13 +23,14 @@ class EthGetCode:
     }
 
     @staticmethod
-    def getCode(_addr: str, _id: int) -> evmDasm.EvmBytecode:
+    def getCode(_addr: str, _id: int) -> evmdasm.EvmInstructions:
         paylode = EthGetCode.__payload
         paylode['id'] = _id
         paylode['params'][0] = _addr
+        logging.info(json.dumps(paylode, indent=4))
         response = requests.post(
             EthGetCode.__url,
-            json=EthGetCode.__payload,
+            json=paylode,
             headers=EthGetCode.__headers)
 
         if response.status_code != 200:
@@ -35,25 +38,68 @@ class EthGetCode:
             raise requests.HTTPError(f"\n{response.status_code = }\n{error['code'] = }\n{error['message'] = }")
 
         byteCode = json.loads(response.text)['result']
-        evmCode = evmDasm.EvmBytecode(byteCode)
+        evmCode = evmdasm.EvmBytecode(byteCode)
         evmInstructions = evmCode.disassemble()
-
-        for instr in evmInstructions:
-            print(instr.name, instr.operand)
 
         return evmInstructions
 
 
 class ByteCodeIO:
+    contractsPath = "contractsParsed.jsonl"
+
     @staticmethod
-    def writeCode(code: evmDasm.EvmBytecode, **kwargs):
-        pass
+    def writeCode(_addr: str, instructions: evmdasm.EvmInstructions, **kwargs):
+        cont = {
+            'Code':
+                [{
+                    'Name': instr.name,
+                    'Operand': instr.operand
+                } for instr in instructions],
+            'Addr': _addr,
+        }
+        for key, value in zip(kwargs.keys(), kwargs.values()):
+            if key in ['Code', 'Addr']:
+                continue
+            cont[key] = value
+
+        # todo: add appending to JsonL file
+        ByteCodeIO.__writeDict(cont)
+        logging.info("Code not completed for writeCode function")
+        exit()
+
+    @staticmethod
+    def __writeDict(data: dict):
+        del data['Code']
+        if not os.path.exists(ByteCodeIO.contractsPath):
+            with open(ByteCodeIO.contractsPath, 'w+') as f:
+                pass
+        with open(ByteCodeIO.contractsPath, 'r+') as contracts:
+            line = contracts.readline()
+
+            if line == '':
+                jout = json.dumps(data)
+                contracts.write(jout + '\n')
+                logging.info("File empty")
+                return
+            found = False
+            while line:
+                if json.loads(line)['Addr'] == data['Addr']:
+                    logging.info("address already found")
+                    jout = json.dumps(data)
+                    contracts.writelines(jout + '\n')
+                    exit(0)
+                else:
+                    line = contracts.readline()
+                    logging.info(f"Cont is not found: {line}")
+                    contracts.writelines(line + '\n')
+                line = contracts.readline()
 
     @staticmethod
     def readCode(path: str):
         pass
 
 
-
 if __name__ == '__main__':
-    EthGetCode.getCode('0x4da27a545c0c5B758a6BA100e3a049001de870f5', 0)
+    addr = '0x4da27a545c0c5B758a6BA100e3a049001de870f5'
+    code = EthGetCode.getCode(addr, 0)
+    ByteCodeIO.writeCode(addr, code)
--- src/codeParsing/main.py
+++ src/codeParsing/main.py
@@ -1,9 +1,24 @@
-from addrLookup import EthGetCode
+from addrLookup import EthGetCode, ByteCodeIO
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
-    # TODO: load in csv of adds
+    addrs = pd.read_csv('../addrs.csv', skiprows=1, usecols=['ContractAddress', 'ContractName'])
+    # print(addrs)
     # TODO: send off to be parsed
+    for i, (addr, name) in addrs.iterrows():
+        i: int = int(i)
+        name: str = str(name)
+        addr: str = str(addr)
+        logging.info(f"{name = }\n{addr = }")
+        # exit(0)
+        # todo check for addr being processed previously
+        byteCode = EthGetCode.getCode(addr, int(i))
+        ByteCodeIO.writeCode(addr, byteCode, Name=name)
+
+        # todo: log addr as parsed
     # TODO: save parse data
     pass
